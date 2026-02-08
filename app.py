9b1f3459ca42c28680dd3658aacc332b1b142146
satheesh22g <satheeshgajula22@gmail.com>
2026-02-08 13:08:03 +0530

    ...

--- app.py
+++ app.py
@@ -138,4 +138,4 @@ def inject_user():
 
 if __name__ == '__main__':
     app.debug = False
-    app.run(host='127.0.0.1', port=8002, use_reloader=False)
+    app.run(host='127.0.0.1', port=8000, use_reloader=False)
